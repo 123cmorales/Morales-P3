0e92ad74ca2077fa8ed9808ce5cea93dacde3d25
Chris <123cmorales@frhsd.com>
2023-02-23 10:56:58 -0500

    Added the first movie with correct vue syntax

--- site.js
+++ site.js
@@ -36,7 +36,7 @@ const vue_app = Vue.createApp({
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
          title : 'IMDB + Chris’s Top 8 Movies',
          owner : 'Chris',
-         url: 'https://github.com/123cmorales/NJIT-3_StarterFiles-master',
+         url: 'https://github.com/123cmorales/Morales-P3',
       }
     },
       methods: {

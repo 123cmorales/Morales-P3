2787319ddd0af525a45c6693f45940b4e723422c
Chris <123cmorales@frhsd.com>
2023-02-23 10:26:38 -0500

    changed project name

--- site.js
+++ site.js
@@ -34,7 +34,9 @@ const vue_app = Vue.createApp({
             // This holds your movies.json data.
             movies: [],
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-         
+         title : 'IMDB + Chris’s Top 8 Movies',
+         owner : 'Chris',
+         url: 'https://github.com/123cmorales/NJIT-3_StarterFiles-master',
       }
     },
       methods: {
@@ -43,3 +45,53 @@ const vue_app = Vue.createApp({
 })
 
 vue_app.mount("#vue_app")
+
+// const app = Vue.createApp({
+//       // data, functions
+//       //template: '<h2>I am the ONE WHO KNOCKS</h2>'
+//       data(){
+//           return {
+//               url: 'https://netninja.dev/',
+//               showBooks: true,
+//               books: [
+//                   {title: 'wow', author: 'bobby', img: 'img/JimmyValmer.png', isFav: true},
+//                   {title: 'eric', author: 'Cartman', img: 'img/KennyMcCormick.png', isFav: false },
+//                   {title: 'Jimmy', author: 'Timmah', img: 'img/Timmy.webp', isFav: true},
+//               ]
+//               // title: 'I am Heisenberg',
+//               // author: 'Walter Heartwell White',
+//               // age: 52,
+//               // x: 0,
+//               // y: 0
+//           }
+//       },
+//       methods: {
+//           toggleShowBooks () {
+//               this.showBooks = !this.showBooks
+//           },
+//           toggleFav(book) {
+//               book.isFav = !book.isFav
+//           }
+//           // handleEvent(e, data) {
+//           //     console.log(e, e.type)
+//           //     if (data) {
+//           //         console.log(data)
+//           //     }
+//           // },
+//           // handleMousemove(e) {
+//           //     this.x = e.offsetX
+//           //     this.y = e.offsetY
+//           // }
+//           //changeTitle(abc) {
+//             // this.title = 'Los Pollos Hermanos'
+//             //this.title = abc;
+//           //}
+//       },
+//       computed: {
+//           filteredBooks(){
+//               return this.books.filter((book) => book.isFav)
+//           }
+//       }
+//   });
+  
+//   app.mount('#app')
